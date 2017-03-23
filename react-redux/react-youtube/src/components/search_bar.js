7bb7a7de750b19e6a57abcdd8e62a877ffc5d8a4
Juan Trelles <juantrelles@gmail.com>
2017-03-23 13:55:06 -0500

    React-YouTube Application
    Sections 1 & 2 from the course Modern React with Redux by Stephen Grider

---
+++ react-redux/react-youtube/src/components/search_bar.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+
+// const SearchBar = () => {
+// 	return <input />;
+// };
+
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { term: '' };
+	}
+
+	render() {
+		return(
+			<div className="search-bar">
+			  <input 
+			  	value = {this.state.term} 
+				onChange={event => this.onInputChange(event.target.value)} />
+			</div>
+		)
+	}
+
+	onInputChange(term) {
+		this.setState({term});
+		this.props.onSearchTermChange(term);
+	}
+}
+
+export default SearchBar;
